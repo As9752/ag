f8b836e9e2e5da4cba6a487f3c330216ca74b7b9
As9752 <72992208+As9752@users.noreply.github.com>
2021-08-29 21:54:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App.js";
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
